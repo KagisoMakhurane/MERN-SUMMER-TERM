256b3a3e2c5210573e090cec7603a40cf7115e87
Kagiso Makhurane <lesegomakhurane@gmail.com>
2026-07-08 16:47:58 +0530

    APIS
    none

---
+++ key-shop-apis/models/Product.js
@@ -0,0 +1,41 @@
+  import mongoose from "mongoose";
+
+  const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    description: {
+      type: String,
+      required: true
+    },
+    price: {
+      type: Number,
+      required: true,
+      min: 0
+    },
+    image: {
+      type: String,
+      required: true
+    },
+    stock: {
+      type: Number,
+      required: true,
+      min: 0
+    }
+  },
+  {
+    timestamps: true,
+    toJSON: {
+      virtuals: true,
+      versionKey: false,
+      transform(doc, object) {
+        delete object._id;
+      }
+    }
+  }
+);
+
+export default mongoose.model("Product", productSchema);
