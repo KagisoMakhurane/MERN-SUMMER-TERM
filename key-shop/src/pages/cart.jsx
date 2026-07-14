33cd4a2b60458661b777ecd0c884d66a67ae8e20
Kagiso Makhurane <lesegomakhurane@gmail.com>
2026-07-14 12:42:04 +0530

    Custom Confirmation Dialogue to clear cart and cart item
    none

--- key-shop/src/pages/cart.jsx
+++ key-shop/src/pages/cart.jsx
@@ -1,10 +1,14 @@
 import {Link} from "react-router-dom";
+import {useState} from "react";
 
-function Cart({cartitems, increaseQuantity, decreaseQuantity,clearCart}) {
+
+function Cart({cartitems, increaseQuantity, decreaseQuantity,clearCart}) { 
 
     const totalAmount = cartitems.reduce((total, item) => total + item.price * item.quantity, 0);
      const totalItems = cartitems.reduce((total,item)=>total+item.quantity,0);
-
+    const [showConfirmation, setShowConfirmation] = useState(false);
+    const [showItemConfirmation, setShowItemConfirmation] = useState(false);
+    const [selectedItem, setSelectedItem] = useState(null);
     if(cartitems.length == 0) {
         return ( <section className="cartPage emptyCart">
             <h1>Cart is Empty</h1>
@@ -14,13 +18,28 @@ function Cart({cartitems, increaseQuantity, decreaseQuantity,clearCart}) {
             </section>
         )
     }
+       
+   const handleClearCart = () => {
+    const confirmClear = window.confirm("Are you sure you want to clear the cart?");
+    if (confirmClear) {
+      clearCart();
+    }}
 
+    const handleDecreaseQuantity = (item) => {
+        if (item.quantity === 1) {
+            setSelectedItem(item);
+            setShowItemConfirmation(true);
+        } else {
+            decreaseQuantity(item.id);
+        }
+    }
+        
 
 return (
     <section className="cartPage">
         <div className="cartHeading">
              <h1>Your Shopping Cart</h1>
-             <button className="clearCartBtn" onClick={clearCart}>Clear Cart</button>
+             <button className="clearCartBtn" onClick={() => setShowConfirmation(true)}>Clear Cart</button>
         </div>
     
           <div className="cartLayout">
@@ -36,7 +55,7 @@ return (
                             <p>₹{item.price} each</p>
 
                            <div className="quantityBox">
-                            <button onClick={()=>decreaseQuantity(item.id)} >
+                            <button onClick={()=>handleDecreaseQuantity(item)} >
                                -
                             </button>
                             <span>{item.quantity}</span>
@@ -80,7 +99,49 @@ return (
             </div> 
 
           </div>
-             
+      {/*confirmation Modal for clearing cart*/}
+          {showConfirmation && (
+            <div className="modal">
+                <div className="modal-content">
+                    <h2>Confirm Clear Cart</h2>
+                    <p>Are you sure you want to clear the cart?</p>
+                    
+                    <button onClick={() => {
+                        clearCart();
+                        setShowConfirmation(false); }}
+                        >
+                            Yes
+                        </button>
+
+                    <button onClick={() => setShowConfirmation(false)}>
+                        No
+                    </button>
+                </div>
+            </div>
+          )
+        };
+      {/*confirmation Modal for clearing item*/}
+            {showItemConfirmation && (
+            <div className="modal">
+                <div className="modal-content">
+                    <h2>Confirm Clear Item</h2>
+                    <p>Are you sure you want to remove <strong>{selectedItem?.name}</strong> from the cart?</p>
+                    
+                    <button onClick={() => {
+                        decreaseQuantity(selectedItem.id);
+                        setShowItemConfirmation(false); }}
+                        >
+                            Yes
+                        </button>
+
+                    <button onClick={() => setShowItemConfirmation(false)}>
+                        No
+                    </button>
+                </div>
+            </div>
+          )
+        };
+
               
 
            
